89ce32271b8278e956ee9e0226c664a384885fc9
pete-hulson <pete.hulson@noaa.gov>
2023-12-08 09:16:16 -0900

    add length resampling

--- R/boot_length.R
+++ R/boot_length.R
@@ -3,7 +3,7 @@
 #' @param lfreq_un expanded length frequency data 
 #'
 #' @return
-#' @export
+#' @export boot_length
 #'
 #' @examples
 boot_length <- function(lfreq_un) {
@@ -11,6 +11,6 @@ boot_length <- function(lfreq_un) {
   lfreq_un %>%
     tidytable::mutate(sex_ln = paste0(sex, "-", length)) %>% 
     tidytable::mutate(sex_ln = sample(sex_ln, .N, replace = TRUE), 
-                      .by = c(year, species_code, hauljoin)) %>% 
+                      .by = c(year, species, prime_join)) %>% 
     tidytable::separate(sex_ln, c('sex', 'length'), sep = '-', convert = TRUE)   
 }
