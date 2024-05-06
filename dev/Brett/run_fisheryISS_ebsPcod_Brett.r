dd359b440a17bef90f48f55ca31b3630c43b2342
Brett Stacy <bstacy2@uw.edu>
2024-05-06 16:58:54 -0700

    Give up on connecting to AFKIN through R

--- dev/Brett/run_fisheryISS_ebsPcod_Brett.r
+++ dev/Brett/run_fisheryISS_ebsPcod_Brett.r
@@ -1,1 +1,20 @@
-# 
+# Script for practicing the basics of fishery length composition expansion and ISS calculation for EBS P.cod:#
+
+
+
+
+
+
+
+
+# Connect to the AFKIN data base - can't do this because I don't have access through SQL. I only have access to AKFIN thgouh the online portal.
+
+
+
+
+
+
+
+
+
+
