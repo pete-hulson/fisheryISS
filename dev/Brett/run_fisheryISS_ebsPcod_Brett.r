35d35714fde6d93de5b8590e75df5d5175e10bd3
Brett Stacy <bstacy2@uw.edu>
2024-05-08 15:58:01 -0700

    start reproducing expansion with .RDS data file

--- dev/Brett/run_fisheryISS_ebsPcod_Brett.r
+++ dev/Brett/run_fisheryISS_ebsPcod_Brett.r
@@ -1,13 +1,26 @@
-# Script for practicing the basics of fishery length composition expansion and ISS calculation for EBS P.cod:#
+# Script for practicing the basics of fishery length composition expansion and ISS calculation for EBS P.cod
+# 
+# Import Matt C. .RDS data query that should mimic what Steve's code does. I checked the code he included that produces the data file and it looks good. All that needed to be changed was "region" and "species" in the sql call file. The sql call file appears to be generic to be changed as needed for region and species.
+# The required species and region inputs were from lines 49 and 59 here: https://github.com/afsc-assessments/EBS_PCOD/blob/dfb30720357630e95b2a8d2366afd346c1a7e8a1/2023_ASSESSMENT/Functions/R/MAIN_BS_PCOD.r
+# Pick and choose code from Steve's functions to reproduce length expansion for easiest case
+
+# Libraries from: https://github.com/afsc-assessments/EBS_PCOD/blob/dfb30720357630e95b2a8d2366afd346c1a7e8a1/2023_ASSESSMENT/Functions/R/LENGTH_BY_CATCH_BS_short.r
+library(data.table)
+library(reshape2)
+# library(rgdal)
+library(dplyr)
+library(lubridate)
+library(swo)
+library(vcdExtra)
+library(misty)
 
 
+# Import Matt C. .RDS data
+Dspcomp.matt = readRDS("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/Data/From Matt C/Dspcomp.RDS")
 
-
-
-
-
-
-# Connect to the AFKIN data base - can't do this because I don't have access through SQL. I only have access to AKFIN thgouh the online portal.
+# plug into code from: https://github.com/afsc-assessments/EBS_PCOD/blob/dfb30720357630e95b2a8d2366afd346c1a7e8a1/2023_ASSESSMENT/Functions/R/LENGTH_BY_CATCH_BS_short.r
+Dspcomp=Dspcomp.matt %>% 
+  dplyr::rename_all(toupper) %>% subset(EXTRAPOLATED_WEIGHT > 0 & NUMB > 0)
 
 
 
