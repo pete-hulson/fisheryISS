e796c4e53e4db7cb42dec987077cf38d8a9552c4
Brett Stacy <bstacy2@uw.edu>
2024-05-09 17:03:10 -0700

    add some code to Pcod and start tidytable Rmd

--- dev/Brett/run_fisheryISS_ebsPcod_Brett.r
+++ dev/Brett/run_fisheryISS_ebsPcod_Brett.r
@@ -14,17 +14,20 @@ library(swo)
 library(vcdExtra)
 library(misty)
 
+# code needed to run below code
+
 
 # Import Matt C. .RDS data
 Dspcomp.matt = readRDS("C:/Users/bstacy2/OneDrive - UW/UW Postdoc/Data/From Matt C/Dspcomp.RDS")
 
 # plug into code from: https://github.com/afsc-assessments/EBS_PCOD/blob/dfb30720357630e95b2a8d2366afd346c1a7e8a1/2023_ASSESSMENT/Functions/R/LENGTH_BY_CATCH_BS_short.r
+# This code is pulled out from a function so needs some code from other scripts to be run. That code is above.
 Dspcomp=Dspcomp.matt %>% 
   dplyr::rename_all(toupper) %>% subset(EXTRAPOLATED_WEIGHT > 0 & NUMB > 0)
 
-
-
-
+Dspcomp <- subset(Dspcomp,EXTRAPOLATED_WEIGHT > 0)
+Dspcomp<- subset(Dspcomp,NUMB>0)   
+Dspcomp$QUARTER<-trunc((as.numeric(Dspcomp$MONTH)/3)-0.3)+1
 
 
 
